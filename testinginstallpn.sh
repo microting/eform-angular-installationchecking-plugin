fc6bc2c949d57db03e3fd68283b89a45cae76535
René Schultz Madsen <rm@microting.com>
2019-11-29 13:35:57 +0100

    Conforming to the simple standard.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-if [ ! -d "/var/www/microting/" ]; then
-	export plugin_count=0
-else
-	export current_folder=`pwd`
-	cd /var/www/microting
-	export plugin_count=`ls -lah | grep angular | grep plugin | wc -l`
-	cd $current_folder
-fi
-export TAB=$'\t'
-if (( $plugin_count > 1 )); then
-	sed '/\/\/ INSERT ROUTES HERE/i ,{' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i path: "installationchecking-pn",' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/installationchecking-pn/installationchecking-pn.module#InstallationCheckingPnModule"' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
-else
-	sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i path: "installationchecking-pn",' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/installationchecking-pn/installationchecking-pn.module#InstallationCheckingPnModule"' src/app/plugins/plugins.routing.ts -i
-	sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
-	sed "s/\"/'/g" src/app/plugins/plugins.routing.ts -i
-fi
-
+sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i path: "installationchecking-pn",' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/installationchecking-pn/installationchecking-pn.module#InstallationCheckingPnModule"' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i },' src/app/plugins/plugins.routing.ts -i
