55bf17664c3710aab1e0c3d8eafcedab9badeeaf
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-12-03 09:44:24 +0100

    Correcting activation test to follow new way of activating plugin.

--- eform-client/e2e/Tests/installationchecking-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/installationchecking-settings/application-settings.plugins-page.spec.ts
@@ -20,24 +20,27 @@ describe('Application settings page - site header section', function () {
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting InstallationChecking Plugin');
         expect(plugin.version).equal('1.0.0.0');
-        expect(plugin.status).equal('Deaktiveret');
 
     });
 
     it('should activate the plugin', function () {
-        pluginPage.pluginSettingsBtn.click();
-        browser.waitForVisible('#PluginDropDown', 40000);
-        pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
-        pluginPage.saveBtn.click();
+        let plugin = pluginsPage.getFirstPluginRowObj();
+        plugin.activateBtn.click();
+        browser.waitForVisible('#pluginOKBtn', 40000);
+        pluginPage.pluginOKBtn.click();
         browser.pause(50000); // We need to wait 50 seconds for the plugin to create db etc.
         browser.refresh();
 
-        browser.waitForVisible('#plugin-id', 40000);
-        const plugin = pluginsPage.getFirstPluginRowObj();
+        loginPage.login();
+        myEformsPage.Navbar.advancedDropdown();
+        myEformsPage.Navbar.clickonSubMenuItem('Plugins');
+        browser.waitForExist('#plugin-name', 50000);
+        browser.pause(10000);
+
+        plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting InstallationChecking Plugin');
         expect(plugin.version).equal('1.0.0.0');
-        expect(plugin.status).equal('Aktiveret');
         expect(browser.element(`//*[contains(@class, 'dropdown')]//*[contains(text(), 'Aftaler')]`).isExisting()).equal(true);
     });
 });
