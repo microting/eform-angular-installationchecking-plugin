fbf2d6507660dedddb9ee7922385c44560b75450
René Schultz Madsen <rm@microting.com>
2019-12-12 08:28:34 +0100

    Fixing the broken naming.

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo "################## END GITVERSION ##################"
 su ubuntu -c \
 "dotnet publish eFormAPI/Plugins/InstallationChecking.Pn/InstallationChecking.Pn.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
 
-if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/installationchecking-pn"]; then
+if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/installationchecking-pn" ]; then
 	su ubuntu -c \
 	"rm -fR /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/installationchecking-pn"
 fi
@@ -27,13 +27,13 @@ su ubuntu -c \
 su ubuntu -c \
 "mkdir -p /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/"
 
-if [ -d "/var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection"]; then
+if [ -d "/var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/InstallationChecking" ]; then
 	su ubuntu -c \
-	"rm -fR /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection"
+	"rm -fR /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/InstallationChecking"
 fi
 
 su ubuntu -c \
-"cp -av /var/www/microting/eform-angular-installationchecking-plugin/eFormAPI/Plugins/InstallationChecking.Pn/InstallationChecking.Pn/out /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection"
+"cp -av /var/www/microting/eform-angular-installationchecking-plugin/eFormAPI/Plugins/InstallationChecking.Pn/InstallationChecking.Pn/out /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/InstallationChecking"
 
 
 echo "Recompile angular"
