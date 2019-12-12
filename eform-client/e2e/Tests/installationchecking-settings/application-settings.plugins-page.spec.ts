6154e17a7877bdc9e5c85f9d00645f31c6d8a59a
René Schultz Madsen <rm@microting.com>
2019-12-12 08:05:19 +0100

    Fixing the Microting Customer plugin, which should be Microting Customer Plugin.

--- eform-client/e2e/Tests/installationchecking-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/installationchecking-settings/application-settings.plugins-page.spec.ts
@@ -44,7 +44,7 @@ describe('Application settings page - site header section', function () {
 
     let secondPlugin = pluginsPage.getSecondPluginRowObj();
     expect(secondPlugin.id).equal(2);
-    expect(secondPlugin.name).equal('Microting Customers plugin');
+    expect(secondPlugin.name).equal('Microting Customers Plugin');
     expect(secondPlugin.version).equal('1.0.0.0');
 
     // pluginPage.pluginSettingsBtn.click();
