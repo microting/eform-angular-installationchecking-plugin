d2c23d35e5aa09b5596332536d1650d7227b7e44
René Schultz Madsen <rm@microting.com>
2019-12-12 08:26:34 +0100

    Fixing the install script.

--- install.sh
+++ install.sh
@@ -17,13 +17,13 @@ echo "################## END GITVERSION ##################"
 su ubuntu -c \
 "dotnet publish eFormAPI/Plugins/InstallationChecking.Pn/InstallationChecking.Pn.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
 
-if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/items-installationchecking-pn"]; then
+if [ -d "/var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/installationchecking-pn"]; then
 	su ubuntu -c \
-	"rm -fR /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/items-installationchecking-pn"
+	"rm -fR /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/installationchecking-pn"
 fi
 
 su ubuntu -c \
-"cp -av /var/www/microting/eform-angular-installationchecking-plugin/eform-client/src/app/plugins/modules/items-installationchecking-pn /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/items-installationchecking-pn"
+"cp -av /var/www/microting/eform-angular-installationchecking-plugin/eform-client/src/app/plugins/modules/installationchecking-pn /var/www/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/installationchecking-pn"
 su ubuntu -c \
 "mkdir -p /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/"
 
